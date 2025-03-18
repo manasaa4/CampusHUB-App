464ec9468d26b6e1128f6b7503f5d40de4eae97f
Nagireddy Manasa <22501a1279@pvpsit.ac.in>
2025-03-18 21:31:35 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import Homepage from './pages/Homepage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import StudentDashboard from './pages/student/StudentDashboard';
+import TeacherDashboard from './pages/teacher/TeacherDashboard';
+import LoginPage from './pages/LoginPage';
+import AdminRegisterPage from './pages/admin/AdminRegisterPage';
+import ChooseUser from './pages/ChooseUser';
+import ForgetPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+const App = () => {
+  const { currentRole } = useSelector(state => state.user);
+
+  return (
+    <Router>
+      {currentRole === null &&
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/choose" element={<ChooseUser visitor="normal" />} />
+          <Route path="/chooseasguest" element={<ChooseUser visitor="guest" />} />
+
+          <Route path="/Adminlogin" element={<LoginPage role="Admin" />} />
+          <Route path='/Admin/forgetpassword' element={<ForgetPassword role="Admin" />} />
+          <Route path='/Admin/resetpassword/:id/:token' element={<ResetPassword role="Admin" />} />
+          <Route path="/Studentlogin" element={<LoginPage role="Student" />} />
+          <Route path='/Student/forgetpassword' element={<ForgetPassword role="Student" />} />
+          <Route path='/Student/resetpassword/:id/:token' element={<ResetPassword role="Student" />} />
+          <Route path="/Teacherlogin" element={<LoginPage role="Teacher" />} />
+          <Route path='/Teacher/forgetpassword' element={<ForgetPassword role="Student" />} />
+          <Route path='/Teacher/resetpassword/:id/:token' element={<ResetPassword role="Student" />} />
+          <Route path="/Adminregister" element={<AdminRegisterPage />} />
+
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>}
+
+      {currentRole === "Admin" &&
+        <>
+          <AdminDashboard />
+        </>
+      }
+
+      {currentRole === "Student" &&
+        <>
+          <StudentDashboard />
+        </>
+      }
+
+      {currentRole === "Teacher" &&
+        <>
+          <TeacherDashboard />
+        </>
+      }
+    </Router>
+  )
+}
+
+export default App
